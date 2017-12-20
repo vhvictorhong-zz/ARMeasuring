b7d5dd9c708e2ee9f3f89a18c2350242e3add2b8
victorhong1988 <victorhong1988@gmail.com>
2017-12-20 16:59:20 -0500

    Setup ARKit and views

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        
     }
 
     override func didReceiveMemoryWarning() {
